30fcb7cab497f8dace9560c58d9814c95156085e
enoquetembe <enoquetembetembe10@gmail.com>
2022-10-12 16:12:45 -0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './styles/global.css'
+
+export function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-2xl'>Hello world </h1>
+
+      <button className=' bg-cyan-500 w-[200px]' >Ok</button>
+
+    </>
+  )
+}
+
+
